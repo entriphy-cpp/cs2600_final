cb3abf6ed325407c9376dbcc5d1ed991b7aa7acb
Matthew Finerty <mfinerty@cpp.edu>
2022-12-08 15:52:37 -0800

    player2: Create opcode enum

---
+++ player2/include/opcodes.h
@@ -0,0 +1,5 @@
+enum Opcodes {
+    PRINT,           // Debugging purposes
+    PLAYER_1_ASK,    // p1 -> p2: Ask Player 2 if they want to play
+    PLAYER_2_CONFIRM // p2 -> p1: Player 2 confirms to Player 1 that they want to play
+};
