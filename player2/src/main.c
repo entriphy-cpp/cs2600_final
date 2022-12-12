a9b0a54081758b8a58ea29f02c0c3f11068925ac
Matthew Finerty <mfinerty@cpp.edu>
2022-12-11 21:31:35 -0800

    player2: Receive first player from Player 1

--- player2/src/main.c
+++ player2/src/main.c
@@ -1,19 +1,28 @@
 #include <MQTTClient.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include "game_state.h"
 #include "input.h"
 #include "mqtt_config.h"
 #include "opcodes.h"
 
+char *current_payload = NULL;
+
 int messageReceived(void* context, char* topic, int length, MQTTClient_message* message) {
     if (strcmp(topic, P2_TOPIC) == 0 && message->payloadlen > 0) {
         enum Opcodes op = *(char *)message->payload;
+        current_payload = message->payload;
         switch (op) {
             case PLAYER_1_TIMEOUT:
                 exit(1);
                 break;
         }
     }
+}
+
+void waitForMessage() {
+    while (current_payload == NULL);
 }
 
 int main(int argc, char* argv[]) {
@@ -38,7 +47,7 @@ int main(int argc, char* argv[]) {
     // Ask user if they want to play tic-tac-toe
     int response = booleanInput("Would you like to play tic-tac-toe? (y/n): ");
     if (!response) {
-        // User answered no
+        // User answered no; publish response and quit
         payload_buffer[0] = PLAYER_2_DENY;
         MQTTClient_publish(client, P1_TOPIC, 1, payload_buffer, QOS, 0, &dt);
         MQTTClient_disconnect(client, 10000);
@@ -48,6 +57,17 @@ int main(int argc, char* argv[]) {
         payload_buffer[0] = PLAYER_2_CONFIRM;
         MQTTClient_publish(client, P1_TOPIC, 1, payload_buffer, QOS, 0, &dt);
     }
+
+    // Game state
+    enum GameState game_state = PLAYER_1_DISPLAY_PLAYER;
+
+    // Wait for Player 1 to decide who goes first
+    printf("Waiting for response from Player 1...\n");
+    waitForMessage();
+    pInitGame init = (pInitGame)current_payload;
+    game_state = init->first_player == 1 ? PLAYER_1_MOVING : PLAYER_2_MOVING;
+    printf("%d\n", init->first_player);
+    sleep(3);
 
     MQTTClient_disconnect(client, 10000);
     MQTTClient_destroy(&client);
