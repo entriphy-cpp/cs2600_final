2e5502473ec88466f343702d5128c055493cc093
Matthew Finerty <mfinerty@cpp.edu>
2022-12-11 22:32:03 -0800

    player2: Implement game loop

--- player2/include/opcodes.h
+++ player2/include/opcodes.h
@@ -4,7 +4,9 @@ enum Opcodes {
     PLAYER_2_CONFIRM, // p2 -> p1: Player 2 confirms to Player 1 that they want to play
     PLAYER_2_DENY,    // p2 -> p1: Player 2 does not want to play
     PLAYER_1_TIMEOUT, // p1 -> p2: Play request timeout
-    PLAYER_1_INIT     // p1 -> p2: Send initial game data to Player 2
+    PLAYER_1_INIT,    // p1 -> p2: Send initial game data to Player 2
+    PLAYER_1_MOVE,    // p1 -> p2: Send row and column
+    PLAYER_2_MOVE     // p2 -> p2: Send row and column
 };
 
 typedef struct __attribute__((__packed__)) {
@@ -14,7 +16,7 @@ typedef struct __attribute__((__packed__)) {
 
 typedef struct __attribute__((__packed__)) {
     char op;
-    char x;
-    char y;
-} PlayerMoveOp;
+    char row;
+    char column;
+} PlayerMove, *pPlayerMove;
 
--- player2/src/main.c
+++ player2/src/main.c
@@ -7,6 +7,8 @@
 #include "mqtt_config.h"
 #include "opcodes.h"
 
+// Game state
+enum GameState game_state;
 char *current_payload = NULL;
 enum Mark { empty = ' ', p1 = 'X', p2 = 'O'};
 enum Mark board[3][3] = {
@@ -14,7 +16,7 @@ enum Mark board[3][3] = {
     empty, empty, empty,
     empty, empty, empty
 };
-
+int totalMoves = 0;
 
 int messageReceived(void* context, char* topic, int length, MQTTClient_message* message) {
     if (strcmp(topic, P2_TOPIC) == 0 && message->payloadlen > 0) {
@@ -30,6 +32,80 @@ int messageReceived(void* context, char* topic, int length, MQTTClient_message* 
 
 void waitForMessage() {
     while (current_payload == NULL);
+}
+
+void setState(enum GameState state) {
+    game_state = state;
+    current_payload = NULL;
+}
+
+// Prints the board
+void printBoard() {
+    printf("-------\n");
+    for (int i = 0; i < 3; i++) {
+        printf("|%c|%c|%c|\n", board[i][0], board[i][1], board[i][2]);
+        printf("-------\n");
+    }
+}
+
+// Gets the mark character for the current player (defined in Mark enum)
+char getPlayerMark() {
+    return game_state == PLAYER_1_MOVING ? p1 : p2;
+}
+
+void playerMove(pPlayerMove move) {
+    int row = integerInputRange("Enter row: ", 1, 3);
+    int column = integerInputRange("Enter column: ", 1, 3);
+
+    // Decrement values to make it a valid index
+    row--;
+    column--;
+
+    if (board[row][column] == empty) {
+        board[row][column] = getPlayerMark();
+    } else {
+        printf("There is already a mark at this position.\n");
+        playerMove(move);
+    }
+
+    board[row][column] = p2;
+    move->op = PLAYER_2_MOVE;
+    move->row = row;
+    move->column = column;
+}
+
+// Checks if the specified player has won by having all marks in a specific row
+int checkRow(int row) {
+    char mark = getPlayerMark();
+    return board[row][0] == mark && board[row][1] == mark && board[row][2] == mark;
+}
+
+// Checks if the specified player has won by having all marks in a specific column
+int checkColumn(int column) {
+    char mark = getPlayerMark();
+    return board[0][column] == mark && board[1][column] == mark && board[2][column] == mark;
+}
+
+// Checks if the player has won via diagonal
+int checkDiagonals() {
+    char mark = getPlayerMark();
+    return (board[0][0] == mark && board[1][1] == mark && board[2][2] == mark) ||
+        (board[0][2] == mark && board[1][1] == mark && board[2][0] == mark);
+}
+
+// Returns 1 or 2 if the specified player won (-1 if tie, 0 if no winner)
+void checkWinner() {
+    if (totalMoves == 9) {
+        setState(PLAYER_TIE);
+        return;
+    }
+
+    int rowWin = checkRow(0) || checkRow(1) || checkRow(2);
+    int columnWin = checkColumn(0) || checkColumn(1) || checkColumn(2);
+    int diagonalWin = checkDiagonals();
+    if (rowWin || columnWin || diagonalWin) {
+        game_state = game_state == PLAYER_1_MOVING ? PLAYER_1_WIN : PLAYER_2_WIN;
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -65,15 +141,38 @@ int main(int argc, char* argv[]) {
         MQTTClient_publish(client, P1_TOPIC, 1, payload_buffer, QOS, 0, &dt);
     }
 
-    // Game state
-    enum GameState game_state = PLAYER_1_DISPLAY_PLAYER;
-
     // Wait for Player 1 to decide who goes first
     printf("Waiting for response from Player 1...\n");
     waitForMessage();
     pInitGame init = (pInitGame)current_payload;
-    game_state = init->first_player == 1 ? PLAYER_1_MOVING : PLAYER_2_MOVING;
-    printf("%d\n", init->first_player);
+    setState(init->first_player == 1 ? PLAYER_1_MOVING : PLAYER_2_MOVING);
+
+    // Game loop
+    while (game_state != PLAYER_1_WIN && game_state != PLAYER_2_WIN && game_state != PLAYER_TIE && game_state != PLAYER_1_QUIT && game_state != PLAYER_2_QUIT) {
+        printBoard();
+        printf("=======\n");
+        printf("PLAYER %d MOVE (%c)\n", game_state - 2, getPlayerMark());
+        if (game_state == PLAYER_1_MOVING) {
+            printf("Waiting for Player 1's move...\n");
+            waitForMessage();
+            pPlayerMove move = (pPlayerMove)current_payload;
+            board[move->row][move->column] = p1;
+        } else {
+            PlayerMove move;
+            playerMove(&move);
+            MQTTClient_publish(client, P1_TOPIC, 3, &move, QOS, 0, &dt);
+        }
+        totalMoves++;
+        checkWinner();
+        if (game_state != PLAYER_1_WIN && game_state != PLAYER_2_WIN && game_state != PLAYER_TIE && game_state != PLAYER_1_QUIT && game_state != PLAYER_2_QUIT)
+            setState(game_state == PLAYER_1_MOVING ? PLAYER_2_MOVING : PLAYER_1_MOVING);
+    }
+
+    if (game_state == PLAYER_TIE) {
+        printf("TIE GAME\n");
+    } else {
+        printf("WINNER: PLAYER %d\n", game_state == PLAYER_1_WIN ? 1 : 2);
+    }
     sleep(3);
 
     MQTTClient_disconnect(client, 10000);
